e6eb97799453b98f75f28c7434581ba49558bb73
Salvatoreb27 <138437593+Salvatoreb27@users.noreply.github.com>
2023-07-07 10:03:14 +0200

    Start Project

---
+++ Lumiere-ProjectWork/src/main/java/lumiere/central/LumiereProjectWorkApplication.java
@@ -0,0 +1,13 @@
+package lumiere.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiereProjectWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumiereProjectWorkApplication.class, args);
+	}
+
+}
---
+++ Lumiere-ProjectWork/src/test/java/lumiere/central/LumiereProjectWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package lumiere.central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumiereProjectWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
