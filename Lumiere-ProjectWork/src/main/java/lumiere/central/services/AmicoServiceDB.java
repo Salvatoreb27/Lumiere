a9ff95a2eccf5a4e076cc69b983af1dce096245e
Salvatoreb27 <138437593+Salvatoreb27@users.noreply.github.com>
2023-07-11 17:44:40 +0200

    AmicoServices

--- Lumiere-ProjectWork/src/main/java/lumiere/central/services/AmicoServiceDB.java
+++ Lumiere-ProjectWork/src/main/java/lumiere/central/services/AmicoServiceDB.java
@@ -1,5 +1,47 @@
 package lumiere.central.services;
 
-public class AmicoServiceDB {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lumiere.central.model.Amico;
+import lumiere.central.repository.AmicoRepository;
+
+@Service
+public class AmicoServiceDB implements AmicoService {
+	
+	@Autowired 
+	private AmicoRepository amicorepository;
+	
+	@Override
+	public List<Amico> getAmici() {
+		return (List<Amico>) amicorepository.findAll();
+	}
+
+	@Override
+	public Amico getAmico(Long id) {
+		Optional<Amico> a = amicorepository.findById(id);
+		if (a.isPresent()) {
+			return a.get();
+		}
+		return null;
+	}
+
+	@Override
+	public Long addAmico(Amico amico) {
+			Amico a = amicorepository.save(amico);
+			return a.getId();
+	}
+
+	@Override
+	public boolean deleteAmico(Long id) {
+		if (amicorepository.existsById(id)) {
+			amicorepository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
 
 }
